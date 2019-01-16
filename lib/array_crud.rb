92ceb05c0a4c2329b181f72880213d040903a2b0
Tom Holmes <tholmes59@gmail.com>
2019-01-16 01:23:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  dogs = ["Rover", "Good boy", "Rex", "Buster"]
+  array = ["Rover", "Good boy", "Rex", "Buster"]
 end
 
 def add_element_to_end_of_array(array, element)
-  dogs << "Fido"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
